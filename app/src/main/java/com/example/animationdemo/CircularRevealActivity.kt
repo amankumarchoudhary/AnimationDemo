65cd945653172828fdc199ba12983da21c70350d
amanc <aman.choudhury@e-arc.com>
2019-11-27 19:58:12 +0530

    Circular reveal WIP

---
+++ app/src/main/java/com/example/animationdemo/CircularRevealActivity.kt
@@ -0,0 +1,12 @@
+package com.example.animationdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CircularRevealActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_circular_reveal)
+    }
+}
