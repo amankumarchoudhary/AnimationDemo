a9e888df3fd1ce502be08ff5f4c541c475292a69
amanc <aman.choudhury@e-arc.com>
2019-12-02 11:39:53 +0530

    Variable name SEARCH_ANIMATION_DURATION change to ANIMATION_DURATION.

--- app/src/main/java/com/example/animationdemo/AnimatorSetActivity.kt
+++ app/src/main/java/com/example/animationdemo/AnimatorSetActivity.kt
@@ -19,25 +19,25 @@ class AnimatorSetActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animator_set)
 
-        val SEARCH_ANIMATION_DURATION = 3000L
+        val ANIMATION_DURATION = 3000L
 
         btn_start.setOnClickListener {
 
             val objectAnimatorStartBtn: ObjectAnimator = ObjectAnimator
                 .ofFloat(btn_start, "translationX", 300f)
-            objectAnimatorStartBtn.duration = SEARCH_ANIMATION_DURATION
+            objectAnimatorStartBtn.duration = ANIMATION_DURATION
 
             val objectAnimatorLogo: ObjectAnimator = ObjectAnimator
                 .ofFloat(iv_logo, "alpha", 1f, 0f)
-            objectAnimatorLogo.duration = SEARCH_ANIMATION_DURATION
+            objectAnimatorLogo.duration = ANIMATION_DURATION
 
             val objectAnimatorCancelBtn: ObjectAnimator = ObjectAnimator
                 .ofFloat(btn_cancel, "alpha", 0f, 1f)
-            objectAnimatorCancelBtn.duration = SEARCH_ANIMATION_DURATION
+            objectAnimatorCancelBtn.duration = ANIMATION_DURATION
 
             val objectAnimatorEditText: ObjectAnimator = ObjectAnimator
                 .ofFloat(et_dummy, "alpha", 0f, 1f)
-            objectAnimatorEditText.duration = SEARCH_ANIMATION_DURATION
+            objectAnimatorEditText.duration = ANIMATION_DURATION
 
 
             val animatorSet = AnimatorSet()
@@ -52,20 +52,20 @@ class AnimatorSetActivity : AppCompatActivity() {
         btn_cancel.setOnClickListener {
             val objectAnimatorStartBtn: ObjectAnimator = ObjectAnimator
                 .ofFloat(btn_start, "translationX", 0f)
-            objectAnimatorStartBtn.duration = SEARCH_ANIMATION_DURATION
+            objectAnimatorStartBtn.duration = ANIMATION_DURATION
 
             val objectAnimatorLogo: ObjectAnimator = ObjectAnimator
                 .ofFloat(iv_logo, "alpha", 0f, 1f)
-            objectAnimatorLogo.duration = SEARCH_ANIMATION_DURATION
+            objectAnimatorLogo.duration = ANIMATION_DURATION
 
             val objectAnimatorCancelBtn: ObjectAnimator = ObjectAnimator
                 .ofFloat(btn_cancel, "alpha", 1f, 0f)
-            objectAnimatorCancelBtn.duration = SEARCH_ANIMATION_DURATION
+            objectAnimatorCancelBtn.duration = ANIMATION_DURATION
             objectAnimatorCancelBtn.startDelay=5000L  //To delay the animation this cancel button
 
             val objectAnimatorEditText: ObjectAnimator = ObjectAnimator
                 .ofFloat(et_dummy, "alpha", 1f, 0f)
-            objectAnimatorEditText.duration = SEARCH_ANIMATION_DURATION
+            objectAnimatorEditText.duration = ANIMATION_DURATION
 
 
             val animatorSet = AnimatorSet()
